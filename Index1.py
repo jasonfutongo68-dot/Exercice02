03ee6f8d06b9b649735c5627954a0d7fedc0fe5f
Futongo <jasonfutongo68@email.com>
2026-03-31 10:22:24 -0700

    Mise à jour du fichier Index1.py avec amélioration

--- Index1.py
+++ Index1.py
@@ -1,6 +1,10 @@
 print("Bienvenue !")
-
 a = input("Entrez votre prémier notre: ")
+while a == "" :
+    print("Veillez entrer le premier nombre: ")
+    a = input("Entre votre premier nombre: ")
 b = input("Entrez votre deuxème nombre: ")
-
+while b == "":
+    print("veillez entre votre deuxième nombre: ")
+    b = input("Entrez votre deuxième nombre: ")
 print(f"Le résultat de l'additiion entre {a} et {b} est égal à {int(a) + int(b)}")
