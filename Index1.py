99e496843f5f19732854ca728de732c600fbabb0
Futongo <jasonfutongo68@email.com>
2026-03-31 00:26:03 -0700

    Premier commit

---
+++ Index1.py
@@ -0,0 +1,6 @@
+print("Bienvenue !")
+
+a = input("Entrez votre prémier notre: ")
+b = input("Entrez votre deuxème nombre: ")
+
+print(f"Le résultat de l'additiion entre {a} et {b} est égal à {int(a) + int(b)}")
