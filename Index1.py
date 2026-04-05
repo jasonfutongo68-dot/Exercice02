e93f959127d1aee5b02f06a9fca8ad7dffcda1ab
Futongo <jasonfutongo68@email.com>
2026-04-04 18:18:09 -0700

    Ajout de la modification du messsage de la variale a

--- Index1.py
+++ Index1.py
@@ -1,5 +1,6 @@
 print("Bienvenue !")
-a = input("Entrez votre prémier notre: ")
+
+a = input("Entrez votre prémier nombre: ")
 while a == "" :
     print("Veillez entrer le premier nombre: ")
     a = input("Entre votre premier nombre: ")
@@ -7,4 +8,5 @@ b = input("Entrez votre deuxème nombre: ")
 while b == "":
     print("veillez entre votre deuxième nombre: ")
     b = input("Entrez votre deuxième nombre: ")
+    
 print(f"Le résultat de l'additiion entre {a} et {b} est égal à {int(a) + int(b)}")
