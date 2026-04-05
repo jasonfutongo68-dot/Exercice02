e9053731c4636650637e9dd8624563b4e91c512b
Futongo <jasonfutongo68@email.com>
2026-04-04 18:28:28 -0700

    Correction des fautes d'orthographe dans les messages

--- Index1.py
+++ Index1.py
@@ -2,11 +2,11 @@ print("Bienvenue !")
 
 a = input("Entrez votre prémier nombre: ")
 while a == "" :
-    print("Veillez entrer le premier nombre: ")
+    print("Veuillez entrer le premier nombre: ")
     a = input("Entre votre premier nombre: ")
 b = input("Entrez votre deuxème nombre: ")
 while b == "":
-    print("veillez entre votre deuxième nombre: ")
+    print("veuillez entre votre deuxième nombre: ")
     b = input("Entrez votre deuxième nombre: ")
     
 print(f"Le résultat de l'additiion entre {a} et {b} est égal à {int(a) + int(b)}")
